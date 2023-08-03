2d10e5a9d14246f53dcf434e12e9bd3a88b9b2c7
Justine <ujusine@yahoo.com>
2023-08-03 11:42:09 +0800

    remove fade animations

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,7 +1,6 @@
 import { ReactComponent as Folder } from "../../images/folder.svg";
 import { ReactComponent as Github } from "../../images/github.svg";
 import { ReactComponent as Live } from "../../images/live.svg";
-import Fade from "react-reveal/Fade";
 
 const Project = ({ repo }) => {
   return (
@@ -10,57 +9,49 @@ const Project = ({ repo }) => {
         <span className="text-xs text-title-sm">/ {repo.createdAt}</span>
         <div>
           <div className="flex items-center justify-between h-14">
-            <Fade left delay={400}>
-              <Folder className="h-7 w-7 fill-slate-900 dark:fill-darkBorderHover" />
-            </Fade>
-            <Fade right delay={400}>
-              <ul className="flex gap-x-2">
+            <Folder className="h-7 w-7 fill-slate-900 dark:fill-darkBorderHover" />
+            <ul className="flex gap-x-2">
+              <li>
+                <a
+                  className="py-1 px-2"
+                  href={repo.repoUrl}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Github className="h-5 w-5 fill-slate-900 hover:fill-slate-500 dark:fill-darkBorderHover" />
+                </a>
+              </li>
+              {repo.liveUrl && (
                 <li>
                   <a
                     className="py-1 px-2"
-                    href={repo.repoUrl}
+                    href={repo.liveUrl}
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <Github className="h-5 w-5 fill-slate-900 hover:fill-slate-500 dark:fill-darkBorderHover" />
+                    <Live className="h-5 w-5 fill-slate-900 dark:fill-darkBorderHover" />
                   </a>
                 </li>
-                {repo.liveUrl && (
-                  <li>
-                    <a
-                      className="py-1 px-2"
-                      href={repo.liveUrl}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <Live className="h-5 w-5 fill-slate-900 dark:fill-darkBorderHover" />
-                    </a>
-                  </li>
-                )}
-              </ul>
-            </Fade>
+              )}
+            </ul>
           </div>
           <h5 className="font-semibold dark:text-darkH">{repo.name}</h5>
-          <Fade>
-            <p className="font-josefin text-p mt-2 mb-5 dark:text-darkP text-sm sm:text-base">
-              {repo.body}
-            </p>
-          </Fade>
-          <Fade bottom cascade duration={1000}>
-            <ul className="flex flex-wrap gap-2 w-4/5">
-              {repo.languages.map((language, index) => {
-                return (
-                  <li
-                    key={index}
-                    className="text-xs py-1 px-2 rounded-md font-bold"
-                    style={{ background: language.bg }}
-                  >
-                    {language.name}
-                  </li>
-                );
-              })}
-            </ul>
-          </Fade>
+          <p className="font-josefin text-p mt-2 mb-5 dark:text-darkP text-sm sm:text-base">
+            {repo.body}
+          </p>
+          <ul className="flex flex-wrap gap-2 w-4/5">
+            {repo.languages.map((language, index) => {
+              return (
+                <li
+                  key={index}
+                  className="text-xs py-1 px-2 rounded-md font-bold"
+                  style={{ background: language.bg }}
+                >
+                  {language.name}
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </div>
     </div>
