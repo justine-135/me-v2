245760fcfdf23f4caf0d3c588c8fcd0f93480587
Justine <ujusine@yahoo.com>
2023-08-22 19:21:13 +0800

    refactor code filtering project type, and add challenges as a type

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -9,43 +9,22 @@ import Highlight from "./Highlight";
 const Projects = () => {
   const projects = data.repos;
 
-  const hightlightData = projects.filter((item) => item.type === "highlight");
-  const personalData = projects.filter((item) => item.type === "other");
-
-  const [activeHighlight, setActiveHighlight] = useState(true);
-  const [activePersonal, setActivePersonal] = useState(false);
   const [sortedProjects, setSortedProjects] = useState(null);
+  const [projectType, setProjectType] = useState("highlight");
 
   const [limit, setLimit] = useState(5);
 
   useEffect(() => {
-    if (activeHighlight) {
-      setSortedProjects(
-        hightlightData.sort(
-          (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
-        )
-      );
-      return;
-    }
-    if (activePersonal) {
-      setSortedProjects(
-        personalData.sort(
-          (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
-        )
-      );
-      return;
-    }
-  }, [activeHighlight, activePersonal]);
+    setSortedProjects(
+      projects
+        .filter((item) => item.type === projectType)
+        .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+    );
+  }, [projectType]);
 
-  const handleActiveHighlight = () => {
-    setActiveHighlight(true);
-    setActivePersonal(false);
+  const handleProjectTypeChange = (type) => {
     setLimit(5);
-  };
-  const handleActivePersonal = () => {
-    setActiveHighlight(false);
-    setActivePersonal(true);
-    setLimit(5);
+    setProjectType(type);
   };
 
   return (
@@ -64,34 +43,44 @@ const Projects = () => {
           <div className="flex mb-6 mt-4">
             <button
               className={`pl-1 pr-5 font-bold border-b-4 dark:text-darkH ${
-                activeHighlight
+                projectType === "highlight"
                   ? " border-[#0ea5e9]"
                   : "border-[#d1d5db] dark:border-[#164e63]"
               }`}
-              onClick={handleActiveHighlight}
+              onClick={() => handleProjectTypeChange("highlight")}
             >
               Highlight
             </button>
             <button
               className={`pl-1 pr-5 font-bold border-b-4 dark:text-darkH ${
-                activePersonal
+                projectType === "sides"
                   ? " border-[#0ea5e9]"
                   : "border-[#d1d5db] dark:border-[#164e63]"
               }`}
-              onClick={handleActivePersonal}
+              onClick={() => handleProjectTypeChange("sides")}
             >
               Sides
+            </button>
+            <button
+              className={`pl-1 pr-5 font-bold border-b-4 dark:text-darkH ${
+                projectType === "challenge"
+                  ? " border-[#0ea5e9]"
+                  : "border-[#d1d5db] dark:border-[#164e63]"
+              }`}
+              onClick={() => handleProjectTypeChange("challenge")}
+            >
+              Challenges
             </button>
           </div>
           <div className="flex flex-col items-center gap-10">
             <div
               className={`grid gap-8 w-full ${
-                activePersonal && "md:grid-cols-2"
+                projectType !== "highlight" && "md:grid-cols-2"
               }`}
             >
               {sortedProjects &&
                 sortedProjects.slice(0, limit).map((repo, index) => {
-                  if (activePersonal) {
+                  if (projectType !== "highlight") {
                     return (
                       <Fade bottom key={index}>
                         <Project repo={repo} />
